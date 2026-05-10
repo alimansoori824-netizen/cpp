ac9e66cd2e54d98f1bbd72cda19ea771830dbc63
Ali Mansoori <alimansoori824@gmail.com>
2026-05-10 13:03:16 +0530

    notes and addition

---
+++ 15-7-25.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+/*
+there are 2 types of comments
+1.single line - //
+2.multi line - /* _ */
+/*syntax to print
+cout <<"    ";
+cout is a keyword
+\n is used to seperate line also known as seperator
+\t is used to give space between words of .5 inch also known as tab
+<< is call insertion operator
+; is called semi-colon and it behaves like terminator */
+int main()
+{
+ float a,b;
+ cout<<"enter 1st number";
+ cin>>a;
+ cout<<"enter 2nd number";
+ cin>>b;
+ cout<<"addition is\t"<<a+b;
+}
