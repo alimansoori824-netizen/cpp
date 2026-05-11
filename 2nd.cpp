e88bd9f29d0643ef3c4e1d0b2aadbf5a57fbbcff
Ali Mansoori <alimansoori824@gmail.com>
2026-05-11 16:02:01 +0530

    sum of 3 digits

---
+++ 2nd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+ int a,b,c,d,e;
+ cout<<"999";
+ cin>>a;
+ cout<<"b=a/100";
+ c=a%100;
+ d=c/10;
+ e=c%10;
+ cout<<"sum of 3 digits"<<b+d+e;
+}
+
+
