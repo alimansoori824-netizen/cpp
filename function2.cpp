8c87ae9b41726cb38d08a841022d99dd3b56f030
Ali Mansoori <alimansoori824@gmail.com>
2026-05-16 23:09:15 +0530

    function

---
+++ function2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void sum(){
+    int a,b,c;
+    cout<<"enter 1st number \t";
+    cin>>a;
+    cout<<"enter 2nd number \t";
+    cin>>b;
+    cout<<"sum= \t"<<a+b<<endl;
+    }
+
+
+int main()
+{
+    for(int i=0;i<=10;i++){
+    sum();
+    }
+}
+
